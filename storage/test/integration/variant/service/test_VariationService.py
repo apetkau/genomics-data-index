a25920a5fc0c54a59a30d1a480b1a71d85ab0616
Aaron Petkau <aaron.petkau@gmail.com>
2021-01-31 10:54:43 -0600

    Tested pairwise distances

--- storage/test/integration/variant/service/test_VariationService.py
+++ storage/test/integration/variant/service/test_VariationService.py
@@ -1,3 +1,5 @@
+import math
+
 from os import path, listdir
 from pathlib import Path
 from typing import Dict, Any
@@ -18,24 +20,24 @@ reference_file = data_dir / 'genome.fasta.gz'
 @pytest.fixture
 def setup() -> Dict[str, Any]:
     database = DatabaseConnection('sqlite:///:memory:')
+    reference_service = ReferenceService(database)
+    reference_service.create_reference_genome(reference_file)
+    reference = reference_service.find_reference_genome('genome')
+
+    ref_contigs = {s.sequence_name: s for s in reference.sequences}
 
     return {
         'database': database,
         'reader': SnippyVariantsReader(sample_dirs),
-        'reference_service': ReferenceService(database),
+        'ref_contigs': ref_contigs,
         'variation_service': VariationService(database)
     }
 
 
 def test_insert_variants(setup):
-    setup['reference_service'].create_reference_genome(reference_file)
-    reference = setup['reference_service'].find_reference_genome('genome')
-
-    ref_contigs = {s.sequence_name: s for s in reference.sequences}
-
     core_masks = setup['reader'].get_core_masks()
     var_df = setup['reader'].get_variants_table()
-
+    ref_contigs = setup['ref_contigs']
     session = setup['database'].get_session()
 
     setup['variation_service'].insert_variants(var_df=var_df,
@@ -62,3 +64,24 @@ def test_insert_variants(setup):
     assert 'reference' == v.sequence.sequence_name, 'Sequence name is incorrect'
     assert 5180 == v.sequence.sequence_length, 'Sequence length is incorrect'
     assert 'del' == v.var_type, 'Type is incorrect'
+
+
+def test_pariwise_distance(setup):
+    core_masks = setup['reader'].get_core_masks()
+    var_df = setup['reader'].get_variants_table()
+    ref_contigs = setup['ref_contigs']
+    variation_service = setup['variation_service']
+
+    variation_service.insert_variants(var_df=var_df,
+                                      ref_contigs=ref_contigs,
+                                      core_masks=core_masks)
+
+    distances = variation_service.pairwise_distance(['SampleA', 'SampleC', 'SampleB'])
+
+    expected_distAB = 1
+    expected_distAC = 1
+    expected_distBC = (1 - 17 / 66)
+
+    assert math.isclose(expected_distAB, distances.loc['SampleA', 'SampleB']), 'Incorrect pairwise distance'
+    assert math.isclose(expected_distAC, distances.loc['SampleA', 'SampleC']), 'Incorrect pairwise distance'
+    assert math.isclose(expected_distBC, distances.loc['SampleB', 'SampleC']), 'Incorrect pairwise distance'
