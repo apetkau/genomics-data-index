3284d9486e104765613dffb542f4d4d570fba96f
Aaron Petkau <aaron.petkau@gmail.com>
2021-02-03 05:21:48 -0600

    Added robinson-foulds comparison to test

--- storage/test/integration/variant/__init__.py
+++ storage/test/integration/variant/__init__.py
@@ -4,3 +4,4 @@ from pathlib import Path
 data_dir = Path(path.dirname(__file__), '..', 'data', 'snippy')
 sample_dirs = [data_dir / d for d in listdir(data_dir) if path.isdir(data_dir / d)]
 reference_file = data_dir / 'genome.fasta.gz'
+tree_file = data_dir / 'tree.tre'
