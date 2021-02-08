356a3520773527b5179e46ab9338902eea2c9aaa
Aaron Petkau <aaron.petkau@gmail.com>
2021-02-08 12:09:49 -0600

    Added ability to export alignments

--- storage/cli/variants.py
+++ storage/cli/variants.py
@@ -1,14 +1,19 @@
+from typing import List
 import click
 from os import path, listdir
 from pathlib import Path
 
+from Bio import AlignIO
+
 from storage.variant.service import DatabaseConnection
 from storage.variant.service.ReferenceService import ReferenceService
 from storage.variant.service.VariationService import VariationService
+from storage.variant.service.CoreAlignmentService import CoreAlignmentService
 from storage.variant.VariantsReader import SnippyVariantsReader
 
 database = DatabaseConnection('sqlite:///:memory:')
 reference_service = None
+variation_service = None
 
 
 @click.group()
@@ -18,6 +23,7 @@ reference_service = None
 def main(database_connection, seqrepo_dir):
     global database
     global reference_service
+    global variation_service
 
     click.echo(f'Connecting to database {database_connection}')
     database = DatabaseConnection(database_connection)
@@ -25,11 +31,13 @@ def main(database_connection, seqrepo_dir):
     click.echo(f'Use seqrepo directory {seqrepo_dir}')
     reference_service = ReferenceService(database, seqrepo_dir)
 
+    variation_service = VariationService(database, reference_service)
+
 
 @main.command()
 @click.argument('snippy_dir', type=click.Path(exists=True))
 @click.option('--reference-file', help='Reference genome', type=click.Path(exists=True))
-def load(snippy_dir, reference_file):
+def load(snippy_dir: Path, reference_file: Path):
     snippy_dir = Path(snippy_dir)
     reference_file = Path(reference_file)
     click.echo(f'Loading {snippy_dir}')
@@ -41,9 +49,24 @@ def load(snippy_dir, reference_file):
     core_masks = variants_reader.get_core_masks()
     print(database)
 
-    var_service = VariationService(database, reference_service)
-    var_service.insert_variants(var_df=var_df,
+    variation_service.insert_variants(var_df=var_df,
                                 reference_name='genome',
                                 core_masks=core_masks)
+    click.echo(f'Loaded variants from [{snippy_dir}] into database')
+
+
+@main.command()
+@click.option('--output-file', help='Output file', type=click.Path())
+@click.option('--reference-name', help='Reference genome name', type=str)
+@click.option('--sample', help='Sample to include in alignment (can list more than one).', multiple=True, type=str)
+def alignment(output_file: Path, reference_name: str, sample: List[str]):
+    alignment_service = CoreAlignmentService(database, reference_service)
+
+    alignment_data = alignment_service.construct_alignment(reference_name=reference_name,
+                                          samples=sample,
+                                          include_reference=True)
+
+    with open(output_file, 'w') as f:
+        AlignIO.write(alignment_data, f, 'fasta')
 
 
