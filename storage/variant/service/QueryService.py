b0683e86c4fd5d626c8aba8448b73cae79fb5053
Aaron Petkau <aaron.petkau@gmail.com>
2021-02-19 15:19:20 -0600

    Added a QueryService with placeholders for different types of queries

---
+++ storage/variant/service/QueryService.py
@@ -0,0 +1,77 @@
+from typing import List, Dict, Set
+import abc
+from pathlib import Path
+
+import pandas as pd
+
+
+def verify_columns_match(expected_columns: Set[str], result_df: pd.DataFrame) -> None:
+    actual_columns = set(result_df.columns.tolist())
+    if not expected_columns.issubset(actual_columns):
+        raise Exception('Results table does not contain expected set of columns. '
+                        f'Expected {expected_columns}, actual {actual_columns}')
+
+
+class QueryService(abc.ABC):
+
+    def __init__(self):
+        pass
+
+    # Scenerio 1 (and 2 with proper implementation)
+    @abc.abstractmethod
+    def find_matches_genome_files(self, sample_reads: Dict[str, List[Path]],
+                                  distance_threshold: float = None) -> pd.DataFrame:
+        matches_df = self._find_matches_genome_files_internal(sample_reads, distance_threshold)
+        verify_columns_match({'Type', 'Sample A', 'Sample B', 'Distance'}, matches_df)
+
+        return matches_df
+
+    @abc.abstractmethod
+    def _find_matches_genome_files_internal(self, sample_reads: Dict[str, List[Path]],
+                                            distance_threshold: float = None) -> pd.DataFrame:
+        pass
+
+    # TODO: Scenario 3 (by subtype) later
+
+    # Scenario 4
+    def find_matches(self, samples: List[str],
+                     distance_threshold: float = None) -> pd.DataFrame:
+        if samples is None or len(samples) == 0:
+            raise Exception(f'Cannot find matches of empty samples list')
+
+        matches_df = self._find_matches_internal(samples, distance_threshold)
+        verify_columns_match({'Type', 'Sample A', 'Sample B', 'Distance'}, matches_df)
+
+        return matches_df
+
+    @abc.abstractmethod
+    def _find_matches_internal(self, samples, distance_threshold: float):
+        pass
+
+    # Scenario 5
+    def pairwise_distance(self, samples: List[str]) -> pd.DataFrame:
+        if samples is None or len(samples) == 0:
+            raise Exception(f'Cannot find pairwise distances of empty samples list')
+
+        distance_df = self._pairwise_distance_internal(samples)
+        verify_columns_match({'Type', 'Sample A', 'Sample B', 'Distance'}, distance_df)
+
+        return distance_df
+
+    @abc.abstractmethod
+    def _pairwise_distance_internal(self, samples: List[str]) -> pd.DataFrame:
+        pass
+
+    # TODO: Scenario 6 and 7 later
+    # Scenario 8 in TreeService
+
+    # Scenario 9
+    def differences_between_genomes(self, sample1: str, sample2: str):
+        differences_df = self._differences_between_genomes_internal(sample1, sample2)
+        verify_columns_match({'Type', 'Sample1 unique', 'Sample2 unique'}, differences_df)
+
+        return differences_df
+
+    @abc.abstractmethod
+    def _differences_between_genomes_internal(self, sample1: str, sample2: str):
+        pass
