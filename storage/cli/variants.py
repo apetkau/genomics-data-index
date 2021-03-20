04e8692a3f66e55a0d432a8e42d15d1e4666b05e
Aaron Petkau <aaron.petkau@gmail.com>
2021-03-20 14:52:40 -0500

    Querying for a specific mutation

--- storage/cli/variants.py
+++ storage/cli/variants.py
@@ -18,13 +18,13 @@ from storage.variant.CoreBitMask import CoreBitMask
 from storage.variant.io.SnippyVariantsReader import SnippyVariantsReader
 from storage.variant.io.VcfVariantsReader import VcfVariantsReader
 from storage.variant.service import DatabaseConnection, EntityExistsError
-from storage.variant.service.CoreAlignmentService import CoreAlignmentService
+# from storage.variant.service.CoreAlignmentService import CoreAlignmentService
 from storage.variant.service.MutationQueryService import MutationQueryService, QueryFeatureMutation, \
     MutationQuerySummaries
 from storage.variant.service.ReferenceService import ReferenceService
-from storage.variant.service.SampleSequenceService import SampleSequenceService
+# from storage.variant.service.SampleSequenceService import SampleSequenceService
 from storage.variant.service.SampleService import SampleService
-from storage.variant.service.TreeService import TreeService
+# from storage.variant.service.TreeService import TreeService
 from storage.variant.service.VariationService import VariationService
 from storage.variant.service.KmerService import KmerService
 from storage.variant.index.KmerIndexer import KmerIndexerSourmash, KmerIndexManager
@@ -62,19 +62,18 @@ def main(ctx, database_connection, database_dir, verbose):
     reference_service = ReferenceService(database, filesystem_storage.reference_dir)
 
     sample_service = SampleService(database)
-    sample_sequence_service = SampleSequenceService(database)
+    # sample_sequence_service = SampleSequenceService(database)
     variation_service = VariationService(database_connection=database,
+                                         variation_dir=filesystem_storage.variation_dir,
                                          reference_service=reference_service,
                                          sample_service=sample_service)
-    alignment_service = CoreAlignmentService(database=database,
-                                             reference_service=reference_service,
-                                             variation_service=variation_service,
-                                             sample_sequence_service=sample_sequence_service)
-    tree_service = TreeService(database, reference_service, alignment_service)
-    mutation_query_service = MutationQueryService(tree_service=tree_service,
-                                                  reference_service=reference_service,
-                                                  sample_service=sample_service,
-                                                  sample_sequence_service=sample_sequence_service)
+    # alignment_service = CoreAlignmentService(database=database,
+    #                                          reference_service=reference_service,
+    #                                          variation_service=variation_service,
+    #                                          sample_sequence_service=sample_sequence_service)
+    # tree_service = TreeService(database, reference_service, alignment_service)
+    mutation_query_service = MutationQueryService(reference_service=reference_service,
+                                                  sample_service=sample_service)
 
     kmer_service = KmerService(database_connection=database,
                                sample_service=sample_service)
@@ -84,8 +83,8 @@ def main(ctx, database_connection, database_dir, verbose):
     ctx.obj['kmer_service'] = kmer_service
     ctx.obj['reference_service'] = reference_service
     ctx.obj['variation_service'] = variation_service
-    ctx.obj['alignment_service'] = alignment_service
-    ctx.obj['tree_service'] = tree_service
+    # ctx.obj['alignment_service'] = alignment_service
+    # ctx.obj['tree_service'] = tree_service
     ctx.obj['sample_service'] = sample_service
     ctx.obj['mutation_query_service'] = mutation_query_service
 
@@ -95,7 +94,7 @@ def load_variants_common(ctx, variants_reader, reference_file, input, build_tree
     reference_service = ctx.obj['reference_service']
     variation_service = ctx.obj['variation_service']
     sample_service = ctx.obj['sample_service']
-    tree_service = ctx.obj['tree_service']
+    # tree_service = ctx.obj['tree_service']
 
     try:
         reference_service.add_reference_genome(reference_file)
@@ -106,22 +105,21 @@ def load_variants_common(ctx, variants_reader, reference_file, input, build_tree
     if len(samples_exist) > 0:
         logger.error(f'Samples {samples_exist} already exist, will not load any variants')
     else:
-        var_df = variants_reader.get_variants_table()
-        core_masks = variants_reader.get_core_masks()
+        # var_df = variants_reader.get_variants_table()
+        # core_masks = variants_reader.get_core_masks()
 
         reference_name = get_genome_name(reference_file)
 
-        variation_service.insert_variants(var_df=var_df,
-                                          reference_name=reference_name,
-                                          core_masks=core_masks)
+        variation_service.insert_variants(reference_name=reference_name,
+                                          variants_reader=variants_reader)
         click.echo(f'Loaded variants from [{input}] into database')
 
-        if build_tree:
-            tree_service.rebuild_tree(reference_name=reference_name,
-                                      align_type=align_type,
-                                      num_cores=threads,
-                                      extra_params=extra_tree_params)
-            click.echo('Finished building tree of all samples')
+        # if build_tree:
+        #     tree_service.rebuild_tree(reference_name=reference_name,
+        #                               align_type=align_type,
+        #                               num_cores=threads,
+        #                               extra_params=extra_tree_params)
+        #     click.echo('Finished building tree of all samples')
 
 
 @main.command(name='load-snippy')
@@ -129,14 +127,15 @@ def load_variants_common(ctx, variants_reader, reference_file, input, build_tree
 @click.argument('snippy_dir', type=click.Path(exists=True))
 @click.option('--reference-file', help='Reference genome', required=True, type=click.Path(exists=True))
 @click.option('--build-tree/--no-build-tree', default=False, help='Builds tree of all samples after loading')
-@click.option('--align-type', help=f'The type of alignment to generate', default='core',
-              type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
+# @click.option('--align-type', help=f'The type of alignment to generate', default='core',
+#               type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
 @click.option('--threads', help='Threads for building tree', default=1,
               type=click.IntRange(min=1, max=num_cores))
 @click.option('--extra-tree-params', help='Extra parameters to tree-building software',
               default=None)
-def load_snippy(ctx, snippy_dir: Path, reference_file: Path, build_tree: bool, align_type: str, threads: int,
+def load_snippy(ctx, snippy_dir: Path, reference_file: Path, build_tree: bool, threads: int,
                 extra_tree_params: str):
+    align_type = None
     snippy_dir = Path(snippy_dir)
     reference_file = Path(reference_file)
     click.echo(f'Loading {snippy_dir}')
@@ -153,14 +152,15 @@ def load_snippy(ctx, snippy_dir: Path, reference_file: Path, build_tree: bool, a
 @click.argument('vcf_fofns', type=click.Path(exists=True))
 @click.option('--reference-file', help='Reference genome', required=True, type=click.Path(exists=True))
 @click.option('--build-tree/--no-build-tree', default=False, help='Builds tree of all samples after loading')
-@click.option('--align-type', help=f'The type of alignment to generate', default='core',
-              type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
+# @click.option('--align-type', help=f'The type of alignment to generate', default='core',
+#               type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
 @click.option('--threads', help='Threads for building tree', default=1,
               type=click.IntRange(min=1, max=num_cores))
 @click.option('--extra-tree-params', help='Extra parameters to tree-building software',
               default=None)
-def load_vcf(ctx, vcf_fofns: Path, reference_file: Path, build_tree: bool, align_type: str, threads: int,
+def load_vcf(ctx, vcf_fofns: Path, reference_file: Path, build_tree: bool, threads: int,
              extra_tree_params: str):
+    align_type = None
     reference_file = Path(reference_file)
 
     # Generate empty masks
@@ -261,88 +261,88 @@ def export(ctx, name: List[str], data_type, ascii: bool):
         raise Exception(f'Unknown data_type=[{data_type}]')
 
 
-@main.command()
-@click.pass_context
-@click.option('--output-file', help='Output file', required=True, type=click.Path())
-@click.option('--reference-name', help='Reference genome name', required=True, type=str)
-@click.option('--align-type', help=f'The type of alignment to generate', default='core',
-              type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
-@click.option('--sample', help='Sample to include in alignment (can list more than one).',
-              multiple=True, type=str)
-def alignment(ctx, output_file: Path, reference_name: str, align_type: str, sample: List[str]):
-    alignment_service = ctx.obj['alignment_service']
-    reference_service = ctx.obj['reference_service']
-    sample_service = ctx.obj['sample_service']
+# @main.command()
+# @click.pass_context
+# @click.option('--output-file', help='Output file', required=True, type=click.Path())
+# @click.option('--reference-name', help='Reference genome name', required=True, type=str)
+# @click.option('--align-type', help=f'The type of alignment to generate', default='core',
+#               type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
+# @click.option('--sample', help='Sample to include in alignment (can list more than one).',
+#               multiple=True, type=str)
+# def alignment(ctx, output_file: Path, reference_name: str, align_type: str, sample: List[str]):
+#     alignment_service = ctx.obj['alignment_service']
+#     reference_service = ctx.obj['reference_service']
+#     sample_service = ctx.obj['sample_service']
+#
+#     if not reference_service.exists_reference_genome(reference_name):
+#         logger.error(f'Reference genome [{reference_name}] does not exist')
+#         sys.exit(1)
+#
+#     found_samples = set(sample_service.which_exists(sample))
+#
+#     if len(sample) > 0 and found_samples != set(sample):
+#         logger.error(f'Samples {set(sample) - found_samples} do not exist')
+#         sys.exit(1)
+#
+#     alignment_data = alignment_service.construct_alignment(reference_name=reference_name,
+#                                                            samples=sample,
+#                                                            align_type=align_type,
+#                                                            include_reference=True)
+#
+#     with open(output_file, 'w') as f:
+#         AlignIO.write(alignment_data, f, 'fasta')
+#         click.echo(f'Wrote alignment to [{output_file}]')
 
-    if not reference_service.exists_reference_genome(reference_name):
-        logger.error(f'Reference genome [{reference_name}] does not exist')
-        sys.exit(1)
 
-    found_samples = set(sample_service.which_exists(sample))
-
-    if len(sample) > 0 and found_samples != set(sample):
-        logger.error(f'Samples {set(sample) - found_samples} do not exist')
-        sys.exit(1)
-
-    alignment_data = alignment_service.construct_alignment(reference_name=reference_name,
-                                                           samples=sample,
-                                                           align_type=align_type,
-                                                           include_reference=True)
-
-    with open(output_file, 'w') as f:
-        AlignIO.write(alignment_data, f, 'fasta')
-        click.echo(f'Wrote alignment to [{output_file}]')
-
-
-@main.command()
-@click.pass_context
-@click.option('--output-file', help='Output file', required=True, type=click.Path())
-@click.option('--reference-name', help='Reference genome name', type=str, required=True)
-@click.option('--align-type', help=f'The type of alignment to use for generating the tree', default='core',
-              type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
-@click.option('--tree-build-type', help=f'The type of tree building software', default='iqtree',
-              type=click.Choice(TreeService.TREE_BUILD_TYPES))
-@click.option('--sample', help='Sample to include in tree (can list more than one).',
-              multiple=True, type=str)
-@click.option('--threads', help='Threads for building tree', default=1,
-              type=click.IntRange(min=1, max=num_cores))
-@click.option('--extra-params', help='Extra parameters to tree-building software',
-              default=None)
-def tree(ctx, output_file: Path, reference_name: str, align_type: str,
-         tree_build_type: str, sample: List[str], threads: int, extra_params: str):
-    alignment_service = ctx.obj['alignment_service']
-    tree_service = ctx.obj['tree_service']
-    reference_service = ctx.obj['reference_service']
-    sample_service = ctx.obj['sample_service']
-
-    if not reference_service.exists_reference_genome(reference_name):
-        logger.error(f'Reference genome [{reference_name}] does not exist')
-        sys.exit(1)
-
-    found_samples = set(sample_service.which_exists(sample))
-
-    if len(sample) > 0 and found_samples != set(sample):
-        logger.error(f'Samples {set(sample) - found_samples} do not exist')
-        sys.exit(1)
-
-    if align_type == 'full' and tree_build_type == 'fasttree':
-        logger.error(f'align_type=[{align_type}] is not supported for tree_build_type=[{tree_build_type}]')
-        sys.exit(1)
-
-    alignment_data = alignment_service.construct_alignment(reference_name=reference_name,
-                                                           samples=sample,
-                                                           align_type=align_type,
-                                                           include_reference=True)
-
-    log_file = f'{output_file}.log'
-
-    tree_data, out = tree_service.build_tree(alignment_data, tree_build_type=tree_build_type,
-                                             num_cores=threads, align_type=align_type, extra_params=extra_params)
-    tree_data.write(outfile=output_file)
-    click.echo(f'Wrote tree to [{output_file}]')
-    with open(log_file, 'w') as log:
-        log.write(out)
-        click.echo(f'Wrote log file to [{log_file}]')
+# @main.command()
+# @click.pass_context
+# @click.option('--output-file', help='Output file', required=True, type=click.Path())
+# @click.option('--reference-name', help='Reference genome name', type=str, required=True)
+# @click.option('--align-type', help=f'The type of alignment to use for generating the tree', default='core',
+#               type=click.Choice(CoreAlignmentService.ALIGN_TYPES))
+# @click.option('--tree-build-type', help=f'The type of tree building software', default='iqtree',
+#               type=click.Choice(TreeService.TREE_BUILD_TYPES))
+# @click.option('--sample', help='Sample to include in tree (can list more than one).',
+#               multiple=True, type=str)
+# @click.option('--threads', help='Threads for building tree', default=1,
+#               type=click.IntRange(min=1, max=num_cores))
+# @click.option('--extra-params', help='Extra parameters to tree-building software',
+#               default=None)
+# def tree(ctx, output_file: Path, reference_name: str, align_type: str,
+#          tree_build_type: str, sample: List[str], threads: int, extra_params: str):
+#     alignment_service = ctx.obj['alignment_service']
+#     tree_service = ctx.obj['tree_service']
+#     reference_service = ctx.obj['reference_service']
+#     sample_service = ctx.obj['sample_service']
+#
+#     if not reference_service.exists_reference_genome(reference_name):
+#         logger.error(f'Reference genome [{reference_name}] does not exist')
+#         sys.exit(1)
+#
+#     found_samples = set(sample_service.which_exists(sample))
+#
+#     if len(sample) > 0 and found_samples != set(sample):
+#         logger.error(f'Samples {set(sample) - found_samples} do not exist')
+#         sys.exit(1)
+#
+#     if align_type == 'full' and tree_build_type == 'fasttree':
+#         logger.error(f'align_type=[{align_type}] is not supported for tree_build_type=[{tree_build_type}]')
+#         sys.exit(1)
+#
+#     alignment_data = alignment_service.construct_alignment(reference_name=reference_name,
+#                                                            samples=sample,
+#                                                            align_type=align_type,
+#                                                            include_reference=True)
+#
+#     log_file = f'{output_file}.log'
+#
+#     tree_data, out = tree_service.build_tree(alignment_data, tree_build_type=tree_build_type,
+#                                              num_cores=threads, align_type=align_type, extra_params=extra_params)
+#     tree_data.write(outfile=output_file)
+#     click.echo(f'Wrote tree to [{output_file}]')
+#     with open(log_file, 'w') as log:
+#         log.write(out)
+#         click.echo(f'Wrote log file to [{log_file}]')
 
 
 QUERY_TYPES = ['sample', 'mutation']
