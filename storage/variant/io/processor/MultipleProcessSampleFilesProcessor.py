ca8ba2b27945f495988d4719bfa9fbd8f01dd96f
Aaron Petkau <aaron.petkau@gmail.com>
2021-04-17 02:39:36 -0500

    Using imap instead of map

--- storage/variant/io/processor/MultipleProcessSampleFilesProcessor.py
+++ storage/variant/io/processor/MultipleProcessSampleFilesProcessor.py
@@ -11,22 +11,31 @@ logger = logging.getLogger(__name__)
 
 class MultipleProcessSampleFilesProcessor(SampleFilesProcessor):
 
-    def __init__(self, preprocess_dir: Path, processing_cores: int):
+    def __init__(self, preprocess_dir: Path, processing_cores: int, max_chunk_size=1000):
         super().__init__()
         self._preprocess_dir = preprocess_dir
         self._processing_cores = processing_cores
+        self._max_chunk_size = max_chunk_size
 
     def handle_single_file(self, sample_files: SampleFiles) -> SampleFiles:
         return sample_files.persist(self._preprocess_dir)
+
+    def _get_chunk_size(self):
+        chunk_size = max(1, int(len(self.sample_files_list()) / self._processing_cores))
+        return min(self._max_chunk_size, chunk_size)
 
     def preprocess_files(self) -> Dict[str, SampleFiles]:
         processed_files = {}
 
         logger.debug(f'Starting preprocessing {len(self.sample_files_list())} samples '
                      f'with {self._processing_cores} cores')
+        chunk_size = self._get_chunk_size()
         with mp.Pool(self._processing_cores) as pool:
-            output_files = pool.map(self.handle_single_file, self.sample_files_list())
+            output_files = pool.imap_unordered(self.handle_single_file,
+                                               self.sample_files_list(),
+                                               chunk_size)
             for entry in output_files:
+                logger.debug(f'Finished {entry.sample_name}')
                 processed_files[entry.sample_name] = entry
 
         logger.debug(f'Finished preprocessing {len(self.sample_files_list())} samples')
