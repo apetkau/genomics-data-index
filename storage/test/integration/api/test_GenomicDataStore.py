e00a14324d194497e238909aaead48204b78d15e
Aaron Petkau <aaron.petkau@gmail.com>
2021-04-20 14:01:33 -0500

    A few additional cases for genomic data store

--- storage/test/integration/api/test_GenomicDataStore.py
+++ storage/test/integration/api/test_GenomicDataStore.py
@@ -4,12 +4,18 @@ from storage.api.GenomicDataStore import GenomicDataStore
 def test_summaries_loaded_data(loaded_database_genomic_data_store: GenomicDataStore):
     gds = loaded_database_genomic_data_store
 
+    # Samples
     assert 9 == gds.count_samples()
     assert {'2014C-3598', '2014C-3599', '2014D-0067', '2014D-0068',
             'CFSAN002349', 'CFSAN023463',
             'SampleA', 'SampleB', 'SampleC'} == set(gds.sample_names())
-    assert 112 == gds.count_mutations('genome')
 
+    # References
+    assert 1 == gds.count_references()
+    assert ['genome'] == gds.reference_names()
+
+    # Mutations
+    assert 112 == gds.count_mutations('genome')
     ms = gds.mutations_summary('genome', id_type='spdi')
     assert 112 == len(ms)
     assert 2 == ms.loc['reference:839:1:G']
