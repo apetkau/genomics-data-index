bfd0d181eb9370b546fcce3aab40c48837fcbd24
Aaron Petkau <aaron.petkau@gmail.com>
2021-05-04 17:17:18 -0500

    Moving towards tests working with example data

--- genomics_data_index/test/integration/pipelines/__init__.py
+++ genomics_data_index/test/integration/pipelines/__init__.py
@@ -3,5 +3,8 @@ import os
 
 
 assemblies_dir = Path(__file__).parent / 'data' / 'assemblies'
-assemblies_reference = assemblies_dir / 'reference.fasta'
-assemblies_samples = [assemblies_dir / s for s in os.listdir(assemblies_dir) if s.startswith('Sample')]
+samples_dir = assemblies_dir / 'samples'
+expected_mutations_dir = assemblies_dir / 'expected'
+assemblies_reference = assemblies_dir / 'genome.fasta'
+assemblies_samples = {os.path.splitext(s)[0]: samples_dir / s for s in os.listdir(samples_dir) if s.startswith('Sample')}
+expected_mutations = {os.path.splitext(s)[0]: expected_mutations_dir / f'{s}-mutations.txt' for s in os.listdir(samples_dir) if s.startswith('Sample')}
--- genomics_data_index/test/integration/pipelines/test_SnakemakePipelineExecutor.py
+++ genomics_data_index/test/integration/pipelines/test_SnakemakePipelineExecutor.py
@@ -4,15 +4,19 @@ from tempfile import TemporaryDirectory
 from genomics_data_index.test.integration.pipelines import assemblies_samples, assemblies_reference
 from genomics_data_index.pipelines.SnakemakePipelineExecutor import SnakemakePipelineExecutor
 
-
-def test_create_fofn_file():
+def test_create_fofn_file_single_sample():
     with TemporaryDirectory() as tmp_dir_str:
         tmp_dir = Path(tmp_dir_str)
+        actual_mutations_file = tmp_dir / 'variant' / 'SampleA.vcf.gz'
+        actual_consensus_file = tmp_dir / 'consensus' / 'SampleA.fasta.gz'
+        input_samples = [assemblies_samples['SampleA']]
 
         pipeline_executor = SnakemakePipelineExecutor(working_directory=tmp_dir)
 
-        input_fofn = pipeline_executor.execute(input_files=assemblies_samples,
+        input_fofn = pipeline_executor.execute(input_files=input_samples,
                                                reference_file=assemblies_reference,
                                                ncores=1)
 
         assert input_fofn.exists()
+        assert actual_mutations_file.exists()
+        assert actual_consensus_file.exists()
