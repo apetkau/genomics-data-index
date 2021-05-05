eecdfba8a1a2e49a7f85f26536b197ed6bb6dd5c
Aaron Petkau <aaron.petkau@gmail.com>
2021-05-05 18:47:52 -0500

    Added sketching with sourmash to snakemake pipeline

--- genomics_data_index/pipelines/assembly_input/workflow/scripts/gdi-create-fofn.py
+++ genomics_data_index/pipelines/assembly_input/workflow/scripts/gdi-create-fofn.py
@@ -13,24 +13,31 @@ def remove_suffix(s, suffix):
 # TODO: Instead of parallel lists I should probably have a dictionary keyed by the sample name
 sample_vcfs = snakemake.input.sample_vcfs
 sample_consensus = snakemake.input.sample_consensus
+sample_sketches = snakemake.input.sample_sketches
 
-if len(sample_vcfs) != len(sample_consensus):
+if (len(sample_vcfs) != len(sample_consensus)) and (len(sample_vcfs) != len(sample_sketches)):
     raise Exception(f'sample_vcfs length [{len(sample_vcfs)}] '
-                    f'does not equal sample_consensus length [{len(sample_consensus)}]')
+                    f'does not equal sample_consensus length [{len(sample_consensus)}] or '
+                    f'sample_sketches length [{len(sample_sketches)}]')
 
 with open(snakemake.output[0], 'w') as fh:
-    fh.write('Sample\tVCF\tMask File\n')
+    fh.write('Sample\tVCF\tMask File\tSourmash Sketch\n')
     for idx in range(0, len(sample_consensus)):
         vcf = os.path.abspath(sample_vcfs[idx])
         consensus = os.path.abspath(sample_consensus[idx])
+        sketch = os.path.abspath(sample_sketches[idx])
 
         # I check both sample names to guarantee that files don't get mixed up
         # I don't know enough about snakemake to solve this differently. Ideally I'd like to just pass a dictionary
         # to this script which maps the sample name to the vcf and consensus file
         sample_name_vcf = remove_suffix(os.path.basename(vcf), '.vcf.gz')
         sample_name_consensus = remove_suffix(os.path.basename(consensus), '.fasta.gz')
+        sample_name_sketch = remove_suffix(os.path.basename(sketch), '.sig.gz')
 
         if sample_name_consensus != sample_name_vcf:
             raise Exception(f'VCF=[{vcf}] has a different sample name from consensus=[{consensus}]')
 
-        fh.write(f'{sample_name_vcf}\t{vcf}\t{consensus}\n')
+        if sample_name_vcf != sample_name_sketch:
+            raise Exception(f'VCF=[{vcf}] has a different sample name from sketch=[{sketch}]')
+
+        fh.write(f'{sample_name_vcf}\t{vcf}\t{consensus}\t{sketch}\n')
