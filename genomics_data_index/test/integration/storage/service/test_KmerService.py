52a953851f09f9b5e90aae82952a453e127d9015
Aaron Petkau <aaron.petkau@gmail.com>
2021-05-07 18:46:52 -0500

    Added additional tests

--- genomics_data_index/test/integration/storage/service/test_KmerService.py
+++ genomics_data_index/test/integration/storage/service/test_KmerService.py
@@ -13,3 +13,30 @@ def test_find_matches_all(database: DatabaseConnection, kmer_service_with_data: 
     all_matches_set = kmer_service_with_data.find_matches_within(['SampleA'], distance_threshold=1.0)
 
     assert {sampleA.id, sampleB.id, sampleC.id} == set(all_matches_set)
+
+
+def test_find_matches_all_lower_threshold(database: DatabaseConnection, kmer_service_with_data: KmerService):
+    sampleA = database.get_session().query(Sample).filter(Sample.name == 'SampleA').one()
+    sampleB = database.get_session().query(Sample).filter(Sample.name == 'SampleB').one()
+    sampleC = database.get_session().query(Sample).filter(Sample.name == 'SampleC').one()
+
+    all_matches_set = kmer_service_with_data.find_matches_within(['SampleA'], distance_threshold=0.522)
+
+    assert {sampleA.id, sampleB.id, sampleC.id} == set(all_matches_set)
+
+
+def test_find_matches_two(database: DatabaseConnection, kmer_service_with_data: KmerService):
+    sampleA = database.get_session().query(Sample).filter(Sample.name == 'SampleA').one()
+    sampleC = database.get_session().query(Sample).filter(Sample.name == 'SampleC').one()
+
+    all_matches_set = kmer_service_with_data.find_matches_within(['SampleA'], distance_threshold=0.521)
+
+    assert {sampleA.id, sampleC.id} == set(all_matches_set)
+
+
+def test_find_matches_one(database: DatabaseConnection, kmer_service_with_data: KmerService):
+    sampleA = database.get_session().query(Sample).filter(Sample.name == 'SampleA').one()
+
+    all_matches_set = kmer_service_with_data.find_matches_within(['SampleA'], distance_threshold=0.49)
+
+    assert {sampleA.id} == set(all_matches_set)
