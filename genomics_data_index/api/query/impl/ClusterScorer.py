2e33f43c9541618a4a9048c0447f601a9ee35e07
Aaron Petkau <aaron.petkau@gmail.com>
2021-05-10 20:53:37 -0500

    Allow passing of sample set to cluster scorer

--- genomics_data_index/api/query/impl/ClusterScorer.py
+++ genomics_data_index/api/query/impl/ClusterScorer.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
+from typing import Union
 
-from genomics_data_index.api.query import SamplesQuery
+import pandas as pd
+
+from genomics_data_index.storage.SampleSet import SampleSet
+from genomics_data_index.api.query.SamplesQuery import SamplesQuery
 from genomics_data_index.api.query.impl.TreeSamplesQuery import TreeSamplesQuery
 
 
@@ -11,18 +15,68 @@ class ClusterScorer:
     def __init__(self, universe_samples: TreeSamplesQuery):
         self._universe_samples = universe_samples
 
-    def _score_sample_mrca_jaccard(self, data: SamplesQuery) -> float:
-        mrca_samples = self._universe_samples.isin(data, kind='mrca')
-        return data.sample_set.jaccard_index(mrca_samples.sample_set)
+    def _score_sample_mrca_jaccard(self, data: Union[SamplesQuery, SampleSet]) -> float:
+        if isinstance(data, SamplesQuery):
+            data_set = data.sample_set
+        elif isinstance(data, SampleSet):
+            data_set = data
+        else:
+            raise Exception(f'Invalid type for data={data}. Got type {type(data)}. Expected {SamplesQuery.__name__}'
+                            f' or {SampleSet.__name__}')
 
-    def score_samples(self, data: SamplesQuery, kind: str = 'mrca_jaccard') -> float:
+        mrca_samples = self._universe_samples.isin(data, kind='mrca')
+        return data_set.jaccard_index(mrca_samples.sample_set)
+
+    def score_samples(self, samples: Union[SamplesQuery, SampleSet], kind: str = 'mrca_jaccard') -> float:
+        """
+        Gives a score for how well the passed set of samples is clustered together in a tree.
+
+        :param samples: The samples to score.
+        :param kind: The kind of scoring method to use.
+        :return: A score for how well the passed set of samples is clustered together in a tree.
+        """
         if kind == 'mrca_jaccard':
-            return self._score_sample_mrca_jaccard(data)
+            return self._score_sample_mrca_jaccard(samples)
         else:
             raise Exception(f'kind=[{kind}] is invalid. Must be one of {self.SCORE_KINDS}')
 
+    def score_groupby(self, groupby_column: str, kind: str = 'mrca_jaccard') -> pd.Series:
+        """
+        Gives a score for how well sets of samples defined by the groupby_column cluster together in a tree.
+
+        :param groupby_column: The column in the samples query dataframe to group by. When using this option you likely
+                               will want to join your query with an external data frame defining extra metadata associated
+                               with the samples.
+        :param kind: The kind of scoring method to use.
+        :return: A series of scores (indexed by the groups in the groupby_column) for how well the passed set of samples
+                 is clustered together in a tree.
+        """
+        if kind not in self.SCORE_KINDS:
+            raise Exception(f'kind=[{kind}] is invalid. Must be one of {self.SCORE_KINDS}')
+        else:
+            universe_df = self._universe_samples.toframe()
+
+            if groupby_column not in universe_df:
+                raise Exception(
+                    f'column={groupby_column} not found in data frame from query={self._universe_samples}. '
+                    f'Perhaps you meant to join this query with a dataframe before scoring clusters?')
+
+            groups_sample_sets_df = universe_df[['Sample ID', groupby_column]].groupby(
+                groupby_column).agg(SampleSet)
+            groups_scores_series = groups_sample_sets_df.apply(
+                lambda x: self.score_samples(x['Sample ID']))
+
+            return groups_scores_series
+
     @classmethod
     def create(cls, universe_samples: SamplesQuery) -> ClusterScorer:
+        """
+        Creates a new ClusterScorer used to score sets of samples in a tree based on how well they cluster together.
+
+        :param universe_samples: The set of samples under consideration. This should have a tree attached to it
+                                 (i.e., should be of type TreeSamplesQuery).
+        :return: A new ClusterScorer for the passed samples.
+        """
         if not isinstance(universe_samples, TreeSamplesQuery):
             raise Exception(f'Incorrect type for universe_samples. Got type={type(universe_samples)}, '
                             f'expected type={TreeSamplesQuery.__class__}')
--- genomics_data_index/test/integration/api/query/test_ClusterScorer.py
+++ genomics_data_index/test/integration/api/query/test_ClusterScorer.py
@@ -25,17 +25,35 @@ def test_score_samples_with_mutation_tree(loaded_database_connection: DataIndexC
     cluster_scorer = ClusterScorer.create(query_tree)
     assert math.isclose(1/1, cluster_scorer.score_samples(query_result), rel_tol=1e-3)
 
+    # Test single sample set
+    query_result = query_tree.isa('SampleA')
+    assert 1 == len(query_result)
+    cluster_scorer = ClusterScorer.create(query_tree)
+    assert math.isclose(1/1, cluster_scorer.score_samples(query_result.sample_set), rel_tol=1e-3)
+
     # Test multiple samples
     query_result = query_tree.isin(['SampleB', 'SampleC'])
     assert 2 == len(query_result)
     cluster_scorer = ClusterScorer.create(query_tree)
     assert math.isclose(2/2, cluster_scorer.score_samples(query_result), rel_tol=1e-3)
 
+    # Test multiple samples set
+    query_result = query_tree.isin(['SampleB', 'SampleC'])
+    assert 2 == len(query_result)
+    cluster_scorer = ClusterScorer.create(query_tree)
+    assert math.isclose(2/2, cluster_scorer.score_samples(query_result.sample_set), rel_tol=1e-3)
+
     # Test multiple samples non-perfect score
     query_result = query_tree.isin(['SampleA', 'SampleB'])
     assert 2 == len(query_result)
     cluster_scorer = ClusterScorer.create(query_tree)
     assert math.isclose(2/3, cluster_scorer.score_samples(query_result), rel_tol=1e-3)
+
+    # Test multiple samples set non-perfect score
+    query_result = query_tree.isin(['SampleA', 'SampleB'])
+    assert 2 == len(query_result)
+    cluster_scorer = ClusterScorer.create(query_tree)
+    assert math.isclose(2/3, cluster_scorer.score_samples(query_result.sample_set), rel_tol=1e-3)
 
     # Test multiple samples non-perfect score 2
     query_result = query_tree.isin(['SampleA', 'SampleC'])
@@ -54,3 +72,9 @@ def test_score_samples_with_mutation_tree(loaded_database_connection: DataIndexC
     assert 0 == len(query_result)
     cluster_scorer = ClusterScorer.create(query_tree)
     assert math.isnan(cluster_scorer.score_samples(query_result))
+
+    # # Test no samples set
+    query_result = query_tree.isa('not exists')
+    assert 0 == len(query_result)
+    cluster_scorer = ClusterScorer.create(query_tree)
+    assert math.isnan(cluster_scorer.score_samples(query_result.sample_set))
