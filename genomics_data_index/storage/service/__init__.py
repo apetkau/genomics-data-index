a2377d8fa9329109a8512c0b70c140ec7f9510d0
Aaron Petkau <aaron.petkau@gmail.com>
2021-06-04 12:43:56 -0500

    Fix up database size for sqlite

--- genomics_data_index/storage/service/__init__.py
+++ genomics_data_index/storage/service/__init__.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import logging
 
 import pandas as pd
@@ -14,6 +15,8 @@ logger = logging.getLogger(__name__)
 class DatabaseConnection:
 
     def __init__(self, connection_string: str, database_path_translator: DatabasePathTranslator):
+        self._setup_database_type(connection_string)
+
         engine = create_engine(connection_string, echo=False)
 
         Session = sessionmaker(bind=engine)
@@ -29,42 +32,55 @@ class DatabaseConnection:
 
         Base.metadata.create_all(engine)
 
+    def _setup_database_type(self, connection_string: str) -> None:
+        logger.debug(f'Setup database type: {connection_string}')
+        self._is_sqlite = connection_string.startswith('sqlite')
+
+        if self._is_sqlite:
+            self._sqlite_path = Path(connection_string[len('sqlite:///'):])
+
     def get_session(self):
         return self._session
 
     def get_database_size(self) -> pd.DataFrame:
-        database_name = self._session.bind.url.database
+        if self._is_sqlite:
+            db_size = self._sqlite_path.stat().st_size
+            size_df = pd.DataFrame([['Database', str(self._sqlite_path.name), 'All', db_size, pd.NA, 1]],
+                                   columns=['Type', 'Name', 'Division', 'Data Size', 'Index Size', 'Number of Items'])
+            return size_df
+        else:
+            database_name = self._session.bind.url.database
 
-        table_names = self._session.bind.table_names()
+            table_names = self._session.bind.table_names()
 
-        # TODO: This could maybe be improved to use something more appropriate form SQLAlchemy
-        statement = f'''
-        SELECT table_name AS `Table`,
-        data_length as `Data size`,
-        index_length as `Index size`
-        FROM information_schema.TABLES
-        WHERE table_schema = "{database_name}";
-        '''
-        result = self._session.execute(statement)
-        size_of_table = result.fetchall()
-
-        size_df = pd.DataFrame(size_of_table, columns=['Division', 'Data Size', 'Index Size'])
-        size_df.insert(0, 'Name', database_name)
-        size_df.insert(0, 'Type', 'Database')
-
-        # Get number of rows from tables in database
-        size_df['Number of Items'] = pd.NA
-        for name in table_names:
-            count_statement = f'''
-            SELECT count(*) from {name};
+            # TODO: This could maybe be improved to use something more appropriate form SQLAlchemy
+            statement = f'''
+            SELECT table_name AS `Table`,
+            data_length as `Data size`,
+            index_length as `Index size`
+            FROM information_schema.TABLES
+            WHERE table_schema = "{database_name}";
             '''
-            result = self._session.execute(count_statement)
-            table_counts = result.fetchall()
-            assert 1 == len(table_counts)
+            result = self._session.execute(statement)
+            size_of_table = result.fetchall()
 
-            size_df.loc[size_df['Division'] == name, 'Number of Items'] = table_counts[0][0]
+            size_df = pd.DataFrame(size_of_table, columns=['Division', 'Data Size', 'Index Size'])
+            size_df.insert(0, 'Name', database_name)
+            size_df.insert(0, 'Type', 'Database')
 
-        return size_df
+            # Get number of rows from tables in database
+            size_df['Number of Items'] = pd.NA
+            for name in table_names:
+                count_statement = f'''
+                SELECT count(*) from {name};
+                '''
+                result = self._session.execute(count_statement)
+                table_counts = result.fetchall()
+                assert 1 == len(table_counts)
+
+                size_df.loc[size_df['Division'] == name, 'Number of Items'] = table_counts[0][0]
+
+            return size_df
 
 
 class EntityExistsError(Exception):
