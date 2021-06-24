f87967a9cfe4b1ad2cbc80119fe8517fc2cf2c48
Aaron Petkau <aaron.petkau@gmail.com>
2021-06-24 11:03:23 -0500

    More additions to vcf parsing

--- genomics_data_index/storage/io/mutation/VcfVariantsReader.py
+++ genomics_data_index/storage/io/mutation/VcfVariantsReader.py
@@ -47,15 +47,40 @@ class VcfVariantsReader(NucleotideFeaturesReader):
 
                 ann_headers = [x.strip() for x in ann_info.split('|')]
 
-                # Split up multiple entries in ANN
-                ann_groups = vcf_df['INFO'].map(lambda x: x['ANN']).explode()
-                ann_series = ann_groups.map(lambda x: x.split('|'))
-                ann_series = ann_series.map(lambda x: dict(zip(ann_headers, x)))
-                print(ann_series)
-                ann_df = pd.json_normalize(ann_series).add_prefix('ANN.')
-                print(ann_df)
+                vcf_df_with_keys = vcf_df.copy()
+                vcf_df_with_keys['VARIANT_KEY'] = vcf_df_with_keys.apply(lambda x: f"{x['CHROM']}:{x['POS']}:{x['REF']}:{x['ALT']}",
+                                                     axis='columns')
+                vcf_df_with_keys = vcf_df_with_keys.reset_index().rename({'index': 'original_index'}, axis='columns')
+                vcf_df_with_keys = vcf_df_with_keys.set_index('VARIANT_KEY')
 
-                return ann_df[
+                # Split up multiple entries in ANN
+                ann_groups = vcf_df_with_keys['INFO'].map(lambda x: x['ANN']).explode()
+                print("ANN Groups\n")
+                print(ann_groups)
+                ann_split_fields = ann_groups.map(lambda x: x.split('|'))
+                ann_split_fields = ann_split_fields.map(lambda x: dict(zip(ann_headers, x))).to_frame()
+                ann_split_fields = ann_split_fields.rename({'INFO': 'ANN'}, axis='columns').reset_index()
+
+                def insert_key_to_dictionary(x: pd.Series):
+                    """
+                    I use this to insert the 'VARIANT_KEY' into the 'ANN' dictionary
+                    so that when I run pd.json_normalize I can keep everything linked by the same key
+                    (since json_normalize removes my index in the Series).
+                    :param x: A series of columns.
+                    :return: A series of columns with the 'VARIANT_KEY' inserted into the 'ANN' dictionary.
+                    """
+                    x['ANN']['VARIANT_KEY'] = x['VARIANT_KEY']
+                    return x
+
+                ann_series = ann_split_fields.apply(insert_key_to_dictionary, axis='columns')['ANN']
+                ann_df = pd.json_normalize(ann_series).add_prefix('ANN.').set_index('ANN.VARIANT_KEY')
+
+                # Now join ann_df back to vcf_df_with_keys to recover the original index
+                vcf_df_with_keys = vcf_df_with_keys.merge(ann_df, left_index=True, right_index=True).set_index('original_index')
+                print("vcf_df_with_keys\n")
+                print(vcf_df_with_keys)
+
+                return vcf_df_with_keys[
                     ['ANN.Annotation', 'ANN.Gene_Name', 'ANN.Gene_ID', 'ANN.Feature_Type', 'ANN.HGVS.c', 'ANN.HGVS.p']]
 
         return pd.DataFrame()
