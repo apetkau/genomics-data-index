350afd35c303e1b7372d71a61aecbe8d56e42aa6
Aaron Petkau <aaron.petkau@gmail.com>
2021-07-26 10:59:18 -0500

    Test sample data features snpeff

--- genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleData.py
+++ genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleData.py
@@ -4,10 +4,13 @@ import pytest
 from pathlib import Path
 import logging
 
+from pybedtools import BedTool
+
 from genomics_data_index.storage.io.mutation.SequenceFile import SequenceFile
 from genomics_data_index.storage.MaskedGenomicRegions import MaskedGenomicRegions
 from genomics_data_index.storage.io.mutation.NucleotideSampleData import NucleotideSampleData
 from genomics_data_index.test.integration import snippy_sample_vcfs_dict, snippy_sample_mask_sequences_dict
+from genomics_data_index.test.integration import snpeff_sample_vcfs
 
 
 logger = logging.getLogger(__name__)
@@ -21,6 +24,19 @@ def sample_dataA():
     mask_region.write(tmp_file)
     sample_data = NucleotideSampleData(sample_name='SampleA',
                                        vcf_file=snippy_sample_vcfs_dict['SampleA'],
+                                       vcf_file_index=None,
+                                       mask_bed_file=tmp_file,
+                                       preprocessed=True)
+    return sample_data
+
+
+@pytest.fixture
+def sample_data_sh10_014():
+    tmp_file = Path(tempfile.mktemp())
+    mask_region = MaskedGenomicRegions(BedTool([('NC_011083', 0, 100), ('NC_011083', 199, 250)]))
+    mask_region.write(tmp_file)
+    sample_data = NucleotideSampleData(sample_name='SH10-014',
+                                       vcf_file=snpeff_sample_vcfs['SH10-014'],
                                        vcf_file_index=None,
                                        mask_bed_file=tmp_file,
                                        preprocessed=True)
@@ -90,3 +106,43 @@ def test_read_sample_data_features_with_unknown(sample_dataA):
 
     # One of the missing
     assert ['reference:1:1:?'] == df[df['POS'] == 1]['VARIANT_ID'].tolist()
+
+
+def test_read_sample_data_features_snpeff_with_unknown(sample_data_sh10_014):
+    df = sample_data_sh10_014.read_sample_data_features(include_masked_regions=True).sort_values('POS')
+
+    num_variants = 139
+    num_masked = 151
+    assert num_variants + num_masked == len(df)
+    assert ['SAMPLE', 'CHROM', 'POS', 'REF', 'ALT', 'TYPE', 'FILE', 'VARIANT_ID',
+            'ANN.Allele', 'ANN.Annotation', 'ANN.Annotation_Impact', 'ANN.Gene_Name', 'ANN.Gene_ID',
+            'ANN.Feature_Type', 'ANN.Transcript_BioType', 'ANN.HGVS.c', 'ANN.HGVS.p'] == list(df.columns)
+    assert {'SH10-014'} == set(df['SAMPLE'].tolist())
+
+    # Number actual variants
+    assert num_variants == len(df[df['ALT'] != '?'])
+
+    # Number missing
+    assert num_masked == len(df[df['ALT'] == '?'])
+
+    variant_ids = set(df['VARIANT_ID'].tolist())
+
+    # Test to verify some are mossing
+    assert 'NC_011083:1:1:?' in variant_ids
+    assert 'NC_011083:100:1:?' in variant_ids
+    assert 'NC_011083:101:1:?' not in variant_ids
+    assert 'NC_011083:199:1:?' not in variant_ids
+    assert 'NC_011083:200:1:?' in variant_ids
+    assert 'NC_011083:250:1:?' in variant_ids
+    assert 'NC_011083:251:1:?' not in variant_ids
+
+    # Variant position
+    v = df[df['POS'] == 140658]
+    assert ['NC_011083'] == v['CHROM'].tolist()
+    assert ['C'] == v['REF'].tolist()
+    assert ['A'] == v['ALT'].tolist()
+    assert ['NC_011083:140658:C:A'] == v['VARIANT_ID'].tolist()
+    assert ['missense_variant'] == v['ANN.Annotation'].tolist()
+    assert ['murF'] == v['ANN.Gene_Name'].tolist()
+    assert ['c.497C>A'] == v['ANN.HGVS.c'].tolist()
+    assert ['p.Ala166Glu'] == v['ANN.HGVS.p'].tolist()
