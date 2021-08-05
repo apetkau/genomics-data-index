c2ec16b82ea18251fd30f48d7fbb6a060e92a5d4
Aaron Petkau <aaron.petkau@gmail.com>
2021-08-05 14:54:34 -0500

    Verified I can iterate over data packages of samples and read them independently

--- genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleDataPackageFactory.py
+++ genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleDataPackageFactory.py
@@ -32,6 +32,20 @@ def create_samples_input_file(tmp_dir: Path, sample_dirs: List[Path]) -> Path:
     return file
 
 
+def verify_features_df_for_sample(sample_name: str, features_df: pd.DataFrame) -> None:
+    if sample_name == 'SampleA':
+        assert 482 == len(features_df)
+        assert {'SampleA'} == set(features_df['SAMPLE'].tolist())
+    elif sample_name == 'SampleB':
+        assert 326 == len(features_df)
+        assert {'SampleB'} == set(features_df['SAMPLE'].tolist())
+    elif sample_name == 'SampleC':
+        assert 362 == len(features_df)
+        assert {'SampleC'} == set(features_df['SAMPLE'].tolist())
+    else:
+        raise Exception(f'sample={sample_name} is not expected')
+
+
 def test_snippy_data_package():
     # Test no index unknown
     with TemporaryDirectory() as preprocess_dir:
@@ -96,17 +110,7 @@ def test_snippy_data_package_iter():
             sample_name = list(processed_data_package.sample_names())[0]
             features_df = processed_data_package.get_features_reader().get_features_table()
 
-            if sample_name == 'SampleA':
-                assert 482 == len(features_df)
-                assert {'SampleA'} == set(features_df['SAMPLE'].tolist())
-            elif sample_name == 'SampleB':
-                assert 326 == len(features_df)
-                assert {'SampleB'} == set(features_df['SAMPLE'].tolist())
-            elif sample_name == 'SampleC':
-                assert 362 == len(features_df)
-                assert {'SampleC'} == set(features_df['SAMPLE'].tolist())
-            else:
-                raise Exception(f'sample={sample_name} is not expected')
+            verify_features_df_for_sample(sample_name=sample_name, features_df=features_df)
 
             handled_samples[sample_name] = True
             number_iterations = number_iterations + 1
@@ -167,6 +171,7 @@ def test_vcf_mask_files_data_package(sample_dirs: List[Path]):
 
 
 def test_vcf_mask_files_data_package_iter(sample_dirs: List[Path]):
+    # Test batch size 1
     with TemporaryDirectory() as preprocess_dir:
         preprocess_dir = Path(preprocess_dir)
         input_file = create_samples_input_file(tmp_dir=preprocess_dir, sample_dirs=sample_dirs)
@@ -183,17 +188,7 @@ def test_vcf_mask_files_data_package_iter(sample_dirs: List[Path]):
             sample_name = list(processed_data_package.sample_names())[0]
             features_df = processed_data_package.get_features_reader().get_features_table()
 
-            if sample_name == 'SampleA':
-                assert 482 == len(features_df)
-                assert {'SampleA'} == set(features_df['SAMPLE'].tolist())
-            elif sample_name == 'SampleB':
-                assert 326 == len(features_df)
-                assert {'SampleB'} == set(features_df['SAMPLE'].tolist())
-            elif sample_name == 'SampleC':
-                assert 362 == len(features_df)
-                assert {'SampleC'} == set(features_df['SAMPLE'].tolist())
-            else:
-                raise Exception(f'sample={sample_name} is not expected')
+            verify_features_df_for_sample(sample_name=sample_name, features_df=features_df)
 
             handled_samples[sample_name] = True
             number_iterations = number_iterations + 1
@@ -201,3 +196,48 @@ def test_vcf_mask_files_data_package_iter(sample_dirs: List[Path]):
         assert handled_samples['SampleA']
         assert handled_samples['SampleB']
         assert handled_samples['SampleC']
+
+    # Test batch size 2
+    with TemporaryDirectory() as preprocess_dir:
+        preprocess_dir = Path(preprocess_dir)
+        input_file = create_samples_input_file(tmp_dir=preprocess_dir, sample_dirs=sample_dirs)
+        data_package_factory = NucleotideInputFilesSampleDataPackageFactory(ncores=1, index_unknown=True,
+                                                                            preprocess_dir=preprocess_dir,
+                                                                            input_files_file=input_file)
+        assert 3 == data_package_factory.number_samples()
+
+        handled_samples = {}
+        data_package_iter = data_package_factory.create_data_package_iter(2)
+
+        # Iteration 1
+        data_package = next(data_package_iter)
+
+        assert 2 == len(data_package.sample_names())
+        processed_data_package = data_package.process_all_data()
+        features_df = processed_data_package.get_features_reader().get_features_table()
+
+        sample_names = list(processed_data_package.sample_names())
+        for sample_name in sample_names:
+            features_df_sample = features_df[features_df['SAMPLE'] == sample_name]
+            verify_features_df_for_sample(sample_name=sample_name, features_df=features_df_sample)
+            handled_samples[sample_name] = True
+
+        # Iteration 2
+        data_package = next(data_package_iter)
+
+        assert 1 == len(data_package.sample_names())
+        processed_data_package = data_package.process_all_data()
+        features_df = processed_data_package.get_features_reader().get_features_table()
+
+        sample_names = list(processed_data_package.sample_names())
+        assert 1 == len(sample_names)
+        sample_name = sample_names[0]
+        verify_features_df_for_sample(sample_name=sample_name, features_df=features_df)
+        handled_samples[sample_name] = True
+
+        assert handled_samples['SampleA']
+        assert handled_samples['SampleB']
+        assert handled_samples['SampleC']
+
+        # Make sure no data packages left to iterate over
+        assert next(data_package_iter, None) is None
