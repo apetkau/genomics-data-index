b6d08b2cac02d24621be63f03785ea49fb047809
Aaron Petkau <aaron.petkau@gmail.com>
2021-08-16 00:38:07 -0500

    Added addition test for dataframe

--- genomics_data_index/test/integration/api/query/test_GenomicsDataIndex_samples_query.py
+++ genomics_data_index/test/integration/api/query/test_GenomicsDataIndex_samples_query.py
@@ -3901,6 +3901,47 @@ def test_features_comparison_kindmutations_with_dataframe(loaded_database_connec
     assert 'hgvs_gn:NC_011083:SEHA_RS03545:p.Trp295*' == comparison_df.loc[
         'NC_011083:630556:G:A', 'ID_HGVS_GN.p']
 
+    # Test 3 categories counts on dataframe query: dataframe column groupby
+    df = pd.DataFrame([
+        [sample_sh14_001.id, 'green'],
+        [sample_sh14_014.id, 'red'],
+        [sample_sh10_014.id, 'blue']
+    ], columns=['Sample ID', 'Color'])
+    q = query(loaded_database_connection_annotations, universe='dataframe',
+              data_frame=df, sample_ids_column='Sample ID')
+    comparison_df = q.features_comparison(sample_categories='Color',
+                                          categories_kind='dataframe',
+                                          unit='count')
+    comparison_df = comparison_df.sort_index()
+    assert comparison_df.index.name == 'Mutation'
+    assert ['Sequence', 'Position', 'Deletion', 'Insertion', 'Total',
+            'blue_count', 'green_count', 'red_count',
+            'blue_total', 'green_total', 'red_total',
+            'Annotation', 'Annotation_Impact',
+            'Gene_Name', 'Gene_ID', 'Feature_Type', 'Transcript_BioType',
+            'HGVS.c', 'HGVS.p', 'ID_HGVS.c', 'ID_HGVS.p', 'ID_HGVS_GN.c',
+            'ID_HGVS_GN.p'] == list(comparison_df.columns)
+    assert 177 == len(comparison_df)
+    assert {3} == set(comparison_df['Total'].tolist())
+    assert {1} == set(comparison_df['red_total'].tolist())
+    assert {1} == set(comparison_df['green_total'].tolist())
+    assert {1} == set(comparison_df['blue_total'].tolist())
+    assert 1 == comparison_df.loc['NC_011083:140658:C:A', 'blue_count']
+    assert 1 == comparison_df.loc['NC_011083:140658:C:A', 'red_count']
+    assert 1 == comparison_df.loc['NC_011083:140658:C:A', 'green_count']
+    assert 'hgvs_gn:NC_011083:murF:p.Ala166Glu' == comparison_df.loc[
+        'NC_011083:140658:C:A', 'ID_HGVS_GN.p']
+    assert 1 == comparison_df.loc['NC_011083:4555461:T:TC', 'blue_count']
+    assert 0 == comparison_df.loc['NC_011083:4555461:T:TC', 'red_count']
+    assert 0 == comparison_df.loc['NC_011083:4555461:T:TC', 'green_count']
+    assert 'hgvs_gn:NC_011083:n.4555461_4555462insC' == comparison_df.loc[
+        'NC_011083:4555461:T:TC', 'ID_HGVS_GN.c']
+    assert 0 == comparison_df.loc['NC_011083:630556:G:A', 'blue_count']
+    assert 1 == comparison_df.loc['NC_011083:630556:G:A', 'red_count']
+    assert 1 == comparison_df.loc['NC_011083:630556:G:A', 'green_count']
+    assert 'hgvs_gn:NC_011083:SEHA_RS03545:p.Trp295*' == comparison_df.loc[
+        'NC_011083:630556:G:A', 'ID_HGVS_GN.p']
+
 
 def test_tofeaturesset_all(loaded_database_only_snippy: DataIndexConnection):
     dfA = pd.read_csv(snippy_all_dataframes['SampleA'], sep='\t')
