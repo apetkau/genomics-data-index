39196305df60770fafe8ae3677f93ff903c24c3d
Aaron Petkau <aaron.petkau@gmail.com>
2021-08-23 14:50:01 -0500

    Fixed up creation of snippy mask to subtract out variants in the VCF file

--- genomics_data_index/pipelines/snakemake/main/workflow/scripts/create-snippy-mask.py
+++ genomics_data_index/pipelines/snakemake/main/workflow/scripts/create-snippy-mask.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from genomics_data_index.storage.io.mutation.SequenceFile import SequenceFile
 from genomics_data_index.storage.MaskedGenomicRegions import MaskedGenomicRegions
 
-
 # Generates a mask bed file from a snippy output directory
 
 
@@ -11,8 +10,11 @@ snippy_dir = Path(snakemake.input.snippy_dir)
 output_mask = Path(snakemake.output.mask)
 
 consensus_file = snippy_dir / 'snps.aligned.fa'
+vcf_file = snippy_dir / 'snps.vcf.gz'
 
 name, consensus_records = SequenceFile(consensus_file).parse_sequence_file()
 mask = MaskedGenomicRegions.from_sequences(consensus_records)
+vcf_regions = MaskedGenomicRegions.from_vcf_file(vcf_file)
+mask = mask.subtract(vcf_regions)
 
 mask.write(output_mask)
