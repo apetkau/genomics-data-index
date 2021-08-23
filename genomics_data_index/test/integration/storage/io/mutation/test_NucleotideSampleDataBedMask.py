102b318092a082d4be34e526ad9073e777193b89
Aaron Petkau <aaron.petkau@gmail.com>
2021-08-23 10:01:32 -0500

    Added some additional tests

--- genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleDataBedMask.py
+++ genomics_data_index/test/integration/storage/io/mutation/test_NucleotideSampleDataBedMask.py
@@ -32,3 +32,42 @@ def test_preprocess_mask():
         assert tmp_dir == data_preprocessed.get_mask_file().parent
         assert tmp_dir == vcf_file.parent
         assert tmp_dir == vcf_file_index.parent
+        assert 70 == len(data_preprocessed.get_mask())
+
+
+def test_preprocess_mask_empty():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = Path(tmp_dir)
+        sample_data = NucleotideSampleDataBedMask.create(sample_name='SampleA', vcf_file=sampleA_vcf,
+                                                         mask_bed_file=None)
+        assert not sample_data.is_preprocessed()
+
+        data_preprocessed = sample_data.preprocess(tmp_dir)
+
+        assert data_preprocessed.is_preprocessed()
+        vcf_file, vcf_file_index = data_preprocessed.get_vcf_file()
+        assert tmp_dir == data_preprocessed.get_mask_file().parent
+        assert tmp_dir == vcf_file.parent
+        assert tmp_dir == vcf_file_index.parent
+
+        assert data_preprocessed.get_mask().is_empty()
+
+
+def test_preprocess_other_mask():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = Path(tmp_dir)
+        initial_mask = tmp_dir / 'initial.bed.gz'
+        BedTool([('NC_011083', 0, 100)]).saveas(initial_mask)
+        sample_data = NucleotideSampleDataBedMask.create(sample_name='SampleA', vcf_file=sampleA_vcf,
+                                                         mask_bed_file=initial_mask)
+        assert not sample_data.is_preprocessed()
+
+        data_preprocessed = sample_data.preprocess(tmp_dir)
+
+        assert data_preprocessed.is_preprocessed()
+        vcf_file, vcf_file_index = data_preprocessed.get_vcf_file()
+        assert tmp_dir == data_preprocessed.get_mask_file().parent
+        assert tmp_dir == vcf_file.parent
+        assert tmp_dir == vcf_file_index.parent
+
+        assert 100 == len(data_preprocessed.get_mask())
