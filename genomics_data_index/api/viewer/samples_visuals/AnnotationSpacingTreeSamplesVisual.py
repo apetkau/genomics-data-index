a06c2f97f4f5e8e0cdfee445773b760499474783
Aaron Petkau <aaron.petkau@gmail.com>
2022-01-14 12:59:41 -0600

    Fixed up border width

--- genomics_data_index/api/viewer/samples_visuals/AnnotationSpacingTreeSamplesVisual.py
+++ genomics_data_index/api/viewer/samples_visuals/AnnotationSpacingTreeSamplesVisual.py
@@ -22,7 +22,7 @@ class AnnotationSpacingTreeSamplesVisual(AbstractAnnotationTreeSamplesVisual):
                          legend_fontsize=0,
                          legend_columns={},
                          annotate_kind='r',
-                         annotate_border_width=0,
+                         annotate_border_width=None,
                          annotate_margin=0)
         self._box_width = width
         self._box_height = height
