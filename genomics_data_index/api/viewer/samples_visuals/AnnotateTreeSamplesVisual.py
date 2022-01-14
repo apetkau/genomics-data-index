c23576fad4d777d3e007a213ed7a4aa021657441
Aaron Petkau <aaron.petkau@gmail.com>
2022-01-14 11:31:07 -0600

    Moved some functionality for annotation visuals to abstract class

--- genomics_data_index/api/viewer/samples_visuals/AnnotateTreeSamplesVisual.py
+++ genomics_data_index/api/viewer/samples_visuals/AnnotateTreeSamplesVisual.py
@@ -2,20 +2,20 @@ import copy
 import logging
 from typing import Union, Iterable, Dict, Any
 
-from ete3 import Tree, TreeStyle, TextFace, RectFace, Face, CircleFace
+from ete3 import Tree, TreeStyle, TextFace
 
 from genomics_data_index.api.query.SamplesQuery import SamplesQuery
-from genomics_data_index.api.viewer.TreeSamplesVisual import TreeSamplesVisual
+from genomics_data_index.api.viewer.samples_visuals.AbstractAnnotationTreeSamplesVisual import AbstractAnnotationTreeSamplesVisual
 
 logger = logging.getLogger(__name__)
 
 
-class AnnotateTreeSamplesVisual(TreeSamplesVisual):
+class AnnotateTreeSamplesVisual(AbstractAnnotationTreeSamplesVisual):
     """
     Creates a visual style which adds boxes which are colored based on the presence/absence of samples in the set.
     """
 
-    ANNOTATE_KINDS = TreeSamplesVisual.LEGEND_FACE_KINDS
+    ANNOTATE_KINDS = AbstractAnnotationTreeSamplesVisual.LEGEND_FACE_KINDS
 
     def __init__(self,
                  samples: Union[SamplesQuery, Iterable[str]],
@@ -152,38 +152,3 @@ class AnnotateTreeSamplesVisual(TreeSamplesVisual):
                                unknown_color=self._color_unknown, include_unknown=self._include_unknown,
                                has_unknowns=has_unknowns,
                                kind=self._annotate_kind, tree_style=tree_style)
-
-    def _build_annotate_face(self, width: int, height: int, border_color: str, bgcolor: str,
-                             opacity: float, label: Union[str, Dict[str, Any]] = None) -> Face:
-        if self._annotate_kind == 'rect' or self._annotate_kind == 'rectangle' or self._annotate_kind == 'r':
-            rf = RectFace(width=width, height=height, fgcolor=None, bgcolor=bgcolor, label=label)
-            rf.border.width = self._annotate_border_width
-            rf.margin_top = self._annotate_margin
-            rf.margin_bottom = self._annotate_margin
-            rf.margin_left = self._annotate_margin
-            rf.margin_right = self._annotate_margin
-            rf.border.color = border_color
-            rf.background.color = bgcolor
-            rf.opacity = opacity
-            rf.hz_align = 1
-            rf.vt_align = 1
-            return rf
-        elif self._annotate_kind == 'circle' or self._annotate_kind == 'circ' or self._annotate_kind == 'c':
-            # Make circle radius such that it fits in bounding box defined by width and height
-            min_dimension = min(width, height)
-            radius = min_dimension / 2
-
-            cf = CircleFace(radius=radius, color=bgcolor, label=label)
-            cf.border.width = self._annotate_border_width
-            cf.margin_top = self._annotate_margin
-            cf.margin_bottom = self._annotate_margin
-            cf.margin_left = self._annotate_margin
-            cf.margin_right = self._annotate_margin
-            cf.border.color = border_color
-            cf.opacity = opacity
-            cf.hz_align = 1
-            cf.vt_align = 1
-            return cf
-        else:
-            raise Exception(f'Invalid value for annotate_kind={self._annotate_kind}.'
-                            f' Must be one of {self.LEGEND_FACE_KINDS}')
---
+++ genomics_data_index/api/viewer/samples_visuals/AnnotationSpacingTreeSamplesVisual.py
@@ -0,0 +1,42 @@
+import copy
+import logging
+from typing import Union, Iterable, Dict, Any
+
+from ete3 import Tree, TreeStyle, TextFace, RectFace, Face, CircleFace, TreeNode
+
+from genomics_data_index.api.query.SamplesQuery import SamplesQuery
+from genomics_data_index.api.viewer.samples_visuals.AbstractAnnotationTreeSamplesVisual import \
+    AbstractAnnotationTreeSamplesVisual
+
+logger = logging.getLogger(__name__)
+
+
+class AnnotationSpacingTreeSamplesVisual(AbstractAnnotationTreeSamplesVisual):
+    """
+    Creates a visual style which adds empty spaces in annotation table.
+    """
+
+    def __init__(self, width: int, height: int, annotate_column: int, color: str = None):
+        super().__init__(samples=set(),
+                         legend_nodesize=0,
+                         legend_fontsize=0,
+                         legend_columns={},
+                         annotate_kind='r',
+                         annotate_border_width=None,
+                         annotate_margin=None)
+        self._box_width = width
+        self._box_height = height
+        self._annotate_column = annotate_column
+        self._color = color
+        self._annotation_opacity = 0.0
+        self._annotate_border_color = 'black'
+
+    def apply_visual(self, tree: Tree, tree_style: TreeStyle) -> None:
+        for leaf in tree.iter_leaves():
+            annotate_face = self._build_annotate_face(width=self._box_width, height=self._box_height,
+                                                      border_color=self._annotate_border_color,
+                                                      bgcolor=self._color,
+                                                      opacity=self._annotation_opacity,
+                                                      label=None)
+
+            leaf.add_face(annotate_face, column=self._annotate_column, position='aligned')
