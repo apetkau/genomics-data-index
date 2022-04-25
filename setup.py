acf25dfa4755c7c117b2be4de0a56db66cf28a33
Aaron Petkau <aaron.petkau@canada.ca>
2022-04-25 17:11:16 -0500

    Updated version for release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='genomics-data-index',
-      version='0.6.0.dev2',
+      version='0.6.0',
       description='Indexes genomics data (mutations, kmers, MLST) for fast querying of features.',
       long_description=long_description,
       long_description_content_type="text/markdown",
