bed4bc2f7e5d16be62ab5712023bd5959e56328f
Aaron Petkau <aaron.petkau@canada.ca>
2022-09-23 16:02:19 -0500

    Added configuration for disabling checking of proteins/cds for snpeff

--- genomics_data_index/pipelines/snakemake/main/workflow/scripts/prepare-snpeff-database.py
+++ genomics_data_index/pipelines/snakemake/main/workflow/scripts/prepare-snpeff-database.py
@@ -9,5 +9,7 @@ output_db_conf = Path(snakemake.output.snpeff_db_conf)
 snpeff_db_dir = output_db_conf.parent.absolute()
 
 sequence_file = SequenceFile(input_reference)
-snpeff_database = sequence_file.create_snpeff_database(database_dir=snpeff_db_dir)
+snpeff_database = sequence_file.create_snpeff_database(database_dir=snpeff_db_dir,
+        no_check_protein=snakemake.config["snpeff"]["no_check_protein"],
+        no_check_cds=snakemake.config["snpeff"]["no_check_cds"])
 snpeff_database.write_database_config(output_db_conf)
