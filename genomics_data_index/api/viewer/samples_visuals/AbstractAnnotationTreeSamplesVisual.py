2351200b63540c6bfa154962600ca61abe6b7e7b
Aaron Petkau <aaron.petkau@canada.ca>
2022-12-05 11:12:24 -0600

    Fix typo

--- genomics_data_index/api/viewer/samples_visuals/AbstractAnnotationTreeSamplesVisual.py
+++ genomics_data_index/api/viewer/samples_visuals/AbstractAnnotationTreeSamplesVisual.py
@@ -2,7 +2,7 @@ import abc
 import logging
 from typing import Union, Iterable, Dict, Any, Optional
 
-from genomis_data_index.ete3treeview import RectFace, Face, CircleFace
+from genomics_data_index.ete3treeview import RectFace, Face, CircleFace
 from genomics_data_index.api.query.SamplesQuery import SamplesQuery
 from genomics_data_index.api.viewer.TreeSamplesVisual import TreeSamplesVisual
 
